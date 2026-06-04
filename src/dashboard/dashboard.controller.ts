1ca7152cfeceab84ee6583d1aaec15ccba9459f4
Duru Favour <favourduru24@gmail.com>
2026-06-04 13:53:06 -0700

    new code update

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,20 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
 import { handle } from 'src/common/utils/handle';
 import { LoggerService } from 'src/logger/logger.service';
+import { DashboardService } from './dashboard.service';
+import { AuthUser } from 'src/auth/decorators/user.decorator';
+import type { User } from '@prisma/client';
 
 @Controller('dashboard')
 @Auth()
 export class DashboardController {
 
-    constructor(private readonly logger: LoggerService) {}
+    constructor(private readonly logger: LoggerService, private readonly dashboardService: DashboardService) {}
+    @Get(':workspaceId/stats')
     @ResponseMessage('Workspace tasks stats fetched successfully.')
-    async getWorkspaceTaskStats () {
-    //    return handle(
-    //     this.logger,
-    //     () => {},
-    //     ''
-    //    )
+    async getWorkspaceTaskStats (@AuthUser() user: User, @Param('workspaceId') workspaceId: string, ) {
+       return handle(
+        this.logger,
+        () => this.dashboardService.getWorkspaceStat(user.id, workspaceId),
+        'DashboardController.getStats'
+       )
     }
 }
--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DashboardController } from './dashboard.controller';
 import { DashboardService } from './dashboard.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { LoggerModule } from 'src/logger/logger.module';
+import { TasksModule } from 'src/tasks/tasks.module';
 
 @Module({
   controllers: [DashboardController],
-  providers: [DashboardService]
+  providers: [DashboardService],
+  imports: [PrismaModule, LoggerModule, TasksModule]
 })
 export class DashboardModule {}
